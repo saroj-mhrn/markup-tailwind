7a8cd57c4eb3b81ba34cbf32d3dd3e912b58f7f2
Rupen Dangol <rupen@mayvenstudios.com>
2024-06-04 11:17:48 +0545

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,33 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './src/**/.{html,js}',
-    './src/**/*.html'
-  ],
-  
+  content: ["./src/**/.{html,js}", "./src/**/*.html"],
+
   theme: {
     fontFamily: {
-      sans: ['nunito-sans', ...defaultTheme.fontFamily.sans],
+      sans: ["Plus Jakarta Sans", "sans-serif"],
       mono: ['Roboto Mono', 'SFMono-Regular'],
     },
     extend: {
       colors: {
-        'dark-1': '#0E0F10',
-        'dark-2': '#1F2124',
+        hc: {
+          orange: "#dd4a01",
+          navy: "#221b47",
+          white: "#fff",
+          black: "#000",
+          'light-grey': "#f3f3f5",
+        }
       },
-      boxShadow: {
-        'type-1': '0px 8px 33px rgba(75, 27, 139, 0.15);',
-      }
+
+      fontSize: {
+        xl: ["120px", 1.1],
+        lg: ["58px", 1.1],
+        md: ["38px", 1.1],
+        sm: ["28px", 1.1],
+        'p-lg': ["24px", 1.4],
+        'p-sm': ["14px", 1.4],
+        'ph': ['12px', { lineHeight: '1.1', letterSpacing: '1.1px' }],
+      },
     },
   },
 };
